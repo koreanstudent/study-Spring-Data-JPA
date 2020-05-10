43afa8e8c93f4820c9b871dc6cf503350b32491d
chang <chang@DESKTOP-L2KUJ51>
2020-05-10 16:58:36 +0900

    jpa

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,12 +3,25 @@ package study.datajpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member,Long>{
 
-	// where username =? and age > ?
+	// where username =? and age > ?   
 	List<Member> findByUsernameAndAgeGreaterThan(String username, int age); //인터페이스만 구현하여도 파라미터 명으로 쿼리생성
 	
+	// 쿼리가 복잡해지면 이렇게 사용 권장 정적쿼리.
+	@Query("select m from Member m where m.username = :username and m.age = :age")
+	List<Member> findUser(@Param("username") String username, @Param("age") int age);
+	
+	@Query("select m.username from Member m")
+	List<String> findUsernameList();
+	
+	// dto 조회할때. new - jpql이 제공하는 문법 
+	@Query("select new study.datajpa.dto.MemerDto(m.id, m.username, t.name) from Member m join m.team")
+	List<MemberDto> findMemberDto();
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,7 +3,6 @@ package study.datajpa.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -20,6 +21,7 @@ import study.datajpa.entity.Member;
 class MemberRepositoryTest {
 
 	@Autowired MemberRepository memberRepository;
+	@Autowired TeamRepository teamRepository;
 	
 	@Test
 	public void testMember() {
@@ -49,6 +51,42 @@ class MemberRepositoryTest {
 		assertThat(result.size()).isEqualTo(1);
 		
 	}
+	
+	@Test
+	public void testQuery() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("AAA", 20);
+		
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+		
+		List<Member> result =memberRepository.findUser("AAA", 15);
+		
+		assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+		assertThat(result.get(0).getAge()).isEqualTo(20);
+		assertThat(result.size()).isEqualTo(1);
+		
+	}
+	
+	@Test
+	public void findMemberDto() {
+		
+		Team team = new Team("teamA");
+		teamRepository.save(team);
+		
+		
+		Member m1 = new Member("AAA", 10);
+		m1.setTeam(team);
+		memberRepository.save(m1);
+		
+		List<MemberDto> memberDto =memberRepository.findMemberDto();
+		for(MemberDto dto : memberDto) {
+			System.out.println("dto = " + dto);
+		}
+
+		
+	}
+	
 
 
 }
